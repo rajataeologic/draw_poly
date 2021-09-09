4daf9c697b805d3ffaa1dd1fb1082063194fc6b4
Aeologic <rajat.jain@aeologic.com>
2021-09-09 16:26:53 +0530

    Draw Overlay on Google Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,145 @@
+// import 'package:flutter/material.dart';
+//
+// import 'my_home_page.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 import 'my_home_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Polyline example',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.orange,
       ),
       home: MyHomePage(),
     );
   }
+}
+
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+  // double _originLatitude = 6.5212402, _originLongitude = 3.3679965;
+  // double _destLatitude = 6.849660, _destLongitude = 3.648190;
+  double _originLatitude = 26.4635139, _originLongitude =74.6326585;
+  double _destLatitude = 26.4609487, _destLongitude = 74.6296164;
+  Map<MarkerId, Marker> markers = {};
+  Map<PolylineId, Polyline> polylines = {};
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+  String googleAPiKey = "AIzaSyBSW1ytclOnkiI344M04K0k9vc7nYGoG0U";
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// origin marker
+    _addMarker(LatLng(_originLatitude, _originLongitude), "origin",
+        BitmapDescriptor.defaultMarker);
+
+    /// destination marker
+    _addMarker(LatLng(_destLatitude, _destLongitude), "destination",
+        BitmapDescriptor.defaultMarkerWithHue(90));
+    _getPolyline();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: GoogleMap(
+            initialCameraPosition: CameraPosition(
+                target: LatLng(_originLatitude, _originLongitude), zoom: 15),
+            myLocationEnabled: true,
+            tiltGesturesEnabled: true,
+            compassEnabled: true,
+            scrollGesturesEnabled: true,
+            zoomGesturesEnabled: true,
+            onMapCreated: _onMapCreated,
+            markers: Set<Marker>.of(markers.values),
+            polylines: Set<Polyline>.of(polylines.values),
+          )),
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) async {
+    mapController = controller;
+  }
+
+  _addMarker(LatLng position, String id, BitmapDescriptor descriptor) {
+    MarkerId markerId = MarkerId(id);
+    Marker marker =
+    Marker(markerId: markerId, icon: descriptor, position: position);
+    markers[markerId] = marker;
+  }
+
+  _addPolyLine() {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+        polylineId: id, color: Colors.red, points: polylineCoordinates,width: 2,jointType: JointType.round);
+    polylines[id] = polyline;
+
+    setState(() {});
+  }
+
+  _getPolyline() async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleAPiKey,
+        PointLatLng(_originLatitude, _originLongitude),
+        PointLatLng(_destLatitude, _destLongitude),
+        travelMode: TravelMode.driving,);
+    if (result.points.isNotEmpty) {
+
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+    print(polylineCoordinates);
+    _addPolyLine();
+  }
+
+
+
+
+
+
 }
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,7 +1,12 @@
 import 'package:draw_poly/Loc.dart';
 import 'package:draw_poly/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:math';
+
+import 'package:maps_toolkit/maps_toolkit.dart' as map_tool;
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -12,41 +17,105 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Location? location;
+  List<LatLng> polylineCoordinates = [];
+  List<LatLng> polygonCoords = new List.empty(growable: true);
+  GoogleMapController? _controller;
+  PolylinePoints polylinePoints = PolylinePoints();
+  Map<PolylineId, Polyline> polylines = {};
+  Set<Marker> m = {
+    Marker(
+      markerId: MarkerId("1"),
+      position: LatLng(26.45854065013806, 74.64073777867344),
+    ),
+    Marker(
+        markerId: MarkerId("2"),
+        position: LatLng(26.458310128334023, 74.6482050485709),
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange)),
+    Marker(
+        markerId: MarkerId("3"),
+        position: LatLng(26.470142984568138, 74.64743257237461),
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta)),
+    Marker(
+        markerId: MarkerId("4"),
+        position: LatLng(26.470046943554404, 74.64048028660801),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet))
+  };
+  CameraPosition initialCameraPosition = CameraPosition(
+      target: LatLng(26.45854065013806, 74.64073777867344), zoom: 14.0);
 
-  CameraPosition initialCameraPosition =
-      CameraPosition(target: LatLng(0.0, 0.0));
+  // var polylineCoordinates;
 
   @override
   void initState() {
     super.initState();
-    getUserLoc();
+
+    polygonCoords.add(LatLng(26.45854065013806, 74.64073777867344));
+    polygonCoords.add(LatLng(26.458310128334023, 74.6482050485709));
+    polygonCoords.add(LatLng(26.470142984568138, 74.64743257237461));
+    // polygonCoords.add(LatLng(26.469958360799392, 74.64437484741211));
+    polygonCoords.add(LatLng(26.470046943554404, 74.64048028660801));
+
+    // getUserLoc();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Google MAP Sample"),
-      ),
-      body: location != null
-          ? Column(
-              children: [
-                Expanded(
-                  child: GoogleMap(
-                      initialCameraPosition: initialCameraPosition,
-                      myLocationEnabled: true,
-                      polygons: myPolygon(),
-                      // polylines: myPolyline(),
-                      onMapCreated: (map) {}),
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  child: Text("Click here.."),
-                )
-              ],
+        appBar: AppBar(
+          title: Text("Google MAP Sample"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: GoogleMap(
+                  initialCameraPosition: initialCameraPosition,
+                  myLocationEnabled: true,
+                  onTap: (l) {
+                    print('${l.latitude},${l.longitude}');
+                    polylineCoordinates.clear();
+                    bool s = _checkIfValidMarker(l, polygonCoords);
+                    if (s) {
+                      var d = map_tool.PolygonUtil.distanceToLine(
+                          map_tool.LatLng(l.latitude, l.longitude),
+                          map_tool.LatLng(polygonCoords.elementAt(0).latitude,
+                              polygonCoords.elementAt(0).longitude),
+                          map_tool.LatLng(polygonCoords.elementAt(3).latitude,
+                              polygonCoords.elementAt(3).longitude));
+                      print(d);
+                      LatLng lat = getPointOnLine(d, Direction.W, l);
+
+                      print('${lat.latitude},${lat.longitude}');
+
+                      var dd = map_tool.PolygonUtil.distanceToLine(
+                          map_tool.LatLng(l.latitude, l.longitude),
+                          map_tool.LatLng(polygonCoords.elementAt(1).latitude,
+                              polygonCoords.elementAt(1).longitude),
+                          map_tool.LatLng(polygonCoords.elementAt(2).latitude,
+                              polygonCoords.elementAt(2).longitude));
+
+                      LatLng lat1 = getPointOnLine(dd, Direction.E, l);
+                      print('lat1: ${lat1.latitude},${lat1.longitude}');
+
+                      addPoint(lat, lat1);
+                    }
+                  },
+                  markers: m,
+                  polygons: myPolygon(),
+                  // polylines: myPolyline(),
+
+                  polylines: Set<Polyline>.of(polylines.values),
+                  onMapCreated: (map) {
+                    _controller = map;
+                  }),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Text("Click here.."),
             )
-          : Container(),
-    );
+          ],
+        ));
   }
 
   void getUserLoc() async {
@@ -60,32 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {});
   }
 
-  Set<Polyline> myPolyline() {
-    List<LatLng> polygonCoords = new List.empty(growable: true);
-    polygonCoords.add(LatLng(24.657002173279082, 75.93423843383789));
-    polygonCoords.add(LatLng(24.65651288056014, 75.94084597934571));
-    polygonCoords.add(LatLng(24.643329325667604, 75.94136096347657));
-    polygonCoords.add(LatLng(24.64373002433451, 75.93453813249513));
-    polygonCoords.add(LatLng(24.657002173279082, 75.93423843383789));
-
-    Set<Polyline> polylineSet = new Set();
-    polylineSet.add(Polyline(
-        polylineId: PolylineId("s"),
-        width: 2,
-        patterns: [PatternItem.dash(15), PatternItem.gap(10)],
-        points: polygonCoords,
-        jointType: JointType.round,
-        color: Colors.white));
-    return polylineSet;
-  }
-
   Set<Polygon> myPolygon() {
-    List<LatLng> polygonCoords = new List.empty(growable: true);
-    polygonCoords.add(LatLng(24.657002173279082, 75.93423843383789));
-    polygonCoords.add(LatLng(24.65651288056014, 75.94084597934571));
-    polygonCoords.add(LatLng(24.643329325667604, 75.94136096347657));
-    polygonCoords.add(LatLng(24.647501226954645, 75.94025803488707));
-
     Set<Polygon> polygonSet = new Set();
     polygonSet.add(Polygon(
         polygonId: PolygonId('test'),
@@ -93,12 +137,117 @@ class _MyHomePageState extends State<MyHomePage> {
         fillColor: Colors.green.shade100,
         geodesic: true,
         strokeWidth: 1,
-        consumeTapEvents: true,
-        onTap: () {
-          print("Click");
-        },
         strokeColor: Colors.green));
 
     return polygonSet;
   }
+
+
+  _addPolyLine() {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+        polylineId: id,
+        color: Colors.red,
+        points: polylineCoordinates,
+        width: 2,
+        patterns: [PatternItem.dash(10), PatternItem.gap(10)],
+        jointType: JointType.round);
+    polylines[id] = polyline;
+
+    setState(() {});
+  }
+
+   bool _checkIfValidMarker(LatLng tap, List<LatLng> vertices) {
+    int intersectCount = 0;
+    for (int j = 0; j < vertices.length - 1; j++) {
+      if (rayCastIntersect(tap, vertices[j], vertices[j + 1])) {
+        intersectCount++;
+      }
+    }
+
+    return ((intersectCount % 2) == 1); // odd = inside, even = outside;
+  }
+
+  bool rayCastIntersect(LatLng tap, LatLng vertA, LatLng vertB) {
+    double aY = vertA.latitude;
+    double bY = vertB.latitude;
+    double aX = vertA.longitude;
+    double bX = vertB.longitude;
+    double pY = tap.latitude;
+    double pX = tap.longitude;
+
+    if ((aY > pY && bY > pY) || (aY < pY && bY < pY) || (aX < pX && bX < pX)) {
+      return false; // a and b can't both be above or below pt.y, and a or
+      // b must be east of pt.x
+    }
+
+    double m = (aY - bY) / (aX - bX); // Rise over run
+    double bee = (-aX) * m + aY; // y = mx + b
+    double x = (pY - bee) / m; // algebra is neat!
+
+    return x > pX;
+  }
+
+  LatLng getPointOnLine(var distance, Direction direction, LatLng l) {
+    LatLng lat = new LatLng(0.0, 0.0);
+    switch (direction) {
+      case Direction.E:
+        for (int i = 0; i < distance + 1; i++) {
+          LatLng ll = fromEPosition(l, i / 1000);
+          print('${ll.latitude},${ll.longitude}');
+          print(_checkIfValidMarker(ll, polygonCoords));
+          if (_checkIfValidMarker(ll, polygonCoords)) {
+            lat = ll;
+            continue;
+          } else {
+            print('${ll.latitude},${ll.longitude}');
+            lat = ll;
+          }
+        }
+        break;
+      case Direction.W:
+        for (int i = 0; i < distance + 1; i++) {
+          LatLng ll = fromWPosition(l, i / 1000);
+          print('${ll.latitude},${ll.longitude}');
+          print(_checkIfValidMarker(ll, polygonCoords));
+          if (_checkIfValidMarker(ll, polygonCoords)) {
+            lat = ll;
+            continue;
+          } else {
+            print('${ll.latitude},${ll.longitude}');
+            lat = ll;
+          }
+        }
+        break;
+    }
+    return lat;
+  }
+
+  //West Direction
+  LatLng fromWPosition(LatLng position, double m) {
+    double radiusEarth = 6378;
+    var newLongitude = position.longitude -
+        (m / radiusEarth) * (180 / pi) / cos(position.latitude * pi / 180);
+    return new LatLng(position.latitude, newLongitude);
+  }
+
+  //East Direction
+  LatLng fromEPosition(LatLng position, double m) {
+    double radiusEarth = 6378;
+
+    var newLongitude = position.longitude +
+        (m / radiusEarth) * (180 / pi) / cos(position.latitude * pi / 180);
+    return new LatLng(position.latitude, newLongitude);
+  }
+
+  void addPoint(LatLng l1, LatLng l2) {
+    polylineCoordinates.add(polygonCoords.elementAt(0));
+    polylineCoordinates.add(polygonCoords.elementAt(1));
+    polylineCoordinates.add(l2);
+    polylineCoordinates.add(l1);
+    polylineCoordinates.add(polygonCoords.elementAt(0));
+    _addPolyLine();
+  }
 }
+
+enum Direction { E, W, N, S }
